0793d2f67670191b0e43e9204813a8cb624f06c2
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-02-25 16:46:35 -0300

    aplicando layout (por enquanto), só pra relembrar das coisas

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,20 @@
-function calcularIMC() {
-  const peso = parseFloat(document.querySelector('.peso').value);
-  const altura = parseFloat(document.querySelector('.altura').value);
-  const resultado = document.querySelector('#resultado');
+// function calcularIMC() {
+//   const peso = parseFloat(document.querySelector('.peso').value);
+//   const altura = parseFloat(document.querySelector('.altura').value);
+//   const resultado = document.querySelector('#resultado');
 
-  if (!peso || !altura) {
-    resultado.innerHTML = '<strong>Preencha todos os campos!</strong>';
-    return;
-  }
+//   if (!peso || !altura) {
+//     resultado.innerHTML = '<strong>Preencha todos os campos!</strong>';
+//     return;
+//   }
 
-  const imc = peso / (altura * altura);
-  let classificacao = '';
+//   const imc = peso / (altura * altura);
+//   let classificacao = '';
 
-  if (imc < 18.5) classificacao = 'Abaixo do peso';
-  else if (imc < 25) classificacao = 'Peso normal';
-  else if (imc < 30) classificacao = 'Sobrepeso';
-  else classificacao = 'Obesidade';
+//   if (imc < 18.5) classificacao = 'Abaixo do peso';
+//   else if (imc < 25) classificacao = 'Peso normal';
+//   else if (imc < 30) classificacao = 'Sobrepeso';
+//   else classificacao = 'Obesidade';
 
-  resultado.innerHTML = `<strong>Seu IMC:</strong> ${imc.toFixed(2)}<br><strong>Classificação:</strong> ${classificacao}`;
-}
+//   resultado.innerHTML = `<strong>Seu IMC:</strong> ${imc.toFixed(2)}<br><strong>Classificação:</strong> ${classificacao}`;
+// }
